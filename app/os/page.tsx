bed1071ba43dcb4811e735eae3c519c4b8bf6a9b
davshin000-creator <dav.shin000@gmail.com>
2026-06-28 15:09:52 -0700

    Update page.tsx

--- app/os/page.tsx
+++ app/os/page.tsx
@@ -2,23 +2,29 @@ import OSHeader from "@/components/os/OSHeader";
 import OSSidebar from "@/components/os/OSSidebar";
 import AISidebar from "@/components/os/AISidebar";
 import PropertyHero from "@/components/os/PropertyHero";
+import ExecutiveWorkspace from "@/components/os/ExecutiveWorkspace";
+import DecisionCenter from "@/components/os/DecisionCenter";
 import WorkspaceMain from "@/components/os/WorkspaceMain";
 import ActivityFeed from "@/components/os/ActivityFeed";
-import DecisionCenter from "@/components/os/DecisionCenter";
 
 export default function NestrovaOSPage() {
   return (
     <main className="min-h-screen bg-[#FAFAFA] text-neutral-950">
       <OSHeader />
+
       <div className="mx-auto grid max-w-[1600px] grid-cols-1 lg:grid-cols-[240px_1fr] xl:grid-cols-[240px_1fr_360px]">
         <OSSidebar />
+
         <section className="min-w-0 p-4 md:p-6">
           <div className="grid gap-6">
             <PropertyHero />
+            <ExecutiveWorkspace />
+            <DecisionCenter />
             <WorkspaceMain />
             <ActivityFeed />
           </div>
         </section>
+
         <AISidebar />
       </div>
     </main>
