4a825bb20a80d5911d72b410dc3592c88a7b65f2
davshin000-creator <dav.shin000@gmail.com>
2026-06-29 16:12:41 -0700

    Create index.ts

---
+++ components/ui/index.ts
@@ -0,0 +1,8 @@
+export { default as Button } from "./Button";
+export { default as Card } from "./Card";
+export { default as Badge } from "./Badge";
+export { default as MetricCard } from "./MetricCard";
+export { default as Section } from "./Section";
+export { default as ProgressBar } from "./ProgressBar";
+export { default as Divider } from "./Divider";
+export { default as Avatar } from "./Avatar";
