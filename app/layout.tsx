4f599929a5ecfd69252b7fbdd2a8094068447081
davshin000-creator <dav.shin000@gmail.com>
2026-06-30 10:48:07 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,21 +21,19 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <ClerkProvider>
-  <html
-    lang="en"
-    className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-  >
-    <body className="min-h-full flex flex-col">
-
-      <NestrovaProviders>
-        {children}
-      </NestrovaProviders>
-
-    </body>
-  </html>
-</ClerkProvider>
+      <html
+        lang="en"
+        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      >
+        <body className="min-h-full flex flex-col">
+          <NestrovaProviders>{children}</NestrovaProviders>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
