a3b431cced2fae39115e9e54728333eb6d203a2f
davshin000-creator <dav.shin000@gmail.com>
2026-06-30 10:28:08 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NestrovaProviders from "@/providers/NestrovaProviders";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
