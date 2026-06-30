dffd4a0b57644698266c84dd97924dd1099c6754
davshin000-creator <dav.shin000@gmail.com>
2026-06-30 10:48:24 -0700

    Update page.tsx

--- app/os/page.tsx
+++ app/os/page.tsx
@@ -7,21 +7,18 @@ import RC45Workspace from "@/components/os/RC45Workspace";
 export default function NestrovaOSPage() {
   return (
     <main className="min-h-screen bg-[#FAFAFA] text-neutral-950">
+      <CommandCenter />
+      <OSHeader />
 
-  <CommandCenter />
+      <div className="mx-auto grid max-w-[1600px] grid-cols-1 lg:grid-cols-[240px_1fr] xl:grid-cols-[240px_1fr_360px]">
+        <OSSidebar />
 
-  <OSHeader />
+        <section className="min-w-0 p-4 md:p-6">
+          <RC45Workspace />
+        </section>
 
-  <div className="mx-auto grid max-w-[1600px] grid-cols-1 lg:grid-cols-[240px_1fr] xl:grid-cols-[240px_1fr_360px]">
-
-    <OSSidebar />
-
-    <section className="min-w-0 p-4 md:p-6">
-      <RC45Workspace />
-    </section>
-
-    <AISidebar />
-
-  </div>
-
-</main>
+        <AISidebar />
+      </div>
+    </main>
+  );
+}
